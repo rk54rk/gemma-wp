b43aac9fb1187d31a4f470d5e669bfede0776ca3
Rongkai <rk54rk@hotmail.com>
2014-12-01 16:01:10 +0000

    background layer added

--- wp-content/themes/gemma/bg-layer/bg-layer.php
+++ wp-content/themes/gemma/bg-layer/bg-layer.php
@@ -1,8 +1,21 @@
 <?php
 ?>
 
-<div class="bg-layer">
-<img id="bg-image" width="298" height="300" src="http://localhost:8888/wp-content/uploads/2014/11/photo-5-298x300.jpg">
+<div class="bg-layer" id="bg-layer">
 </div>
 
-<script src="<?php echo get_stylesheet_directory_uri(); ?>/bg-layer/bg-layer.js"></script>
+<style>
+.bg-layer { 
+    height:100%;
+    width:100%;
+	position:fixed;
+	overflow:hidden;
+	margin-top:0px;
+	
+  background: url(<?php echo get_stylesheet_directory_uri(); ?>/bg-layer/02.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+</style>
